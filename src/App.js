9612286ff912c623259a8939f7c2d0db623e2fed
mithunmr888 <mithunmr888@gmail.com>
2024-01-27 14:37:53 +0530

    static modal is working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import React, { Fragment } from "react";
 import MainHeader from "./components/Layout/MainHeader";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 
 function App() {
   return (
     <Fragment>
+      <Cart></Cart>
       <MainHeader></MainHeader>
       <main>
         <Meals></Meals>
